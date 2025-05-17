9bb1649bf693d8b0251e382f038fe07c01a5ceeb
RSL Solution Private Limited <rslsolutionpvtltd@gmail.com>
2025-05-17 13:53:51 +0530

    Added New Class - UBER

---
+++ src/main/java/com/example/Makemytrip/uber.java
@@ -0,0 +1,13 @@
+package com.example.Makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class uber {
+    public uber() {
+    }
+
+    @GetMapping({"/uber"})
+    public String getData() {
+        return "Please book your UBER TAXI AT at 10% discount";
+    }
+}
