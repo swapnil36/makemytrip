7a8ac6c31d23b4dc380aec660ffb930619dcbe16
RSL Solution Private Limited <rslsolutionpvtltd@gmail.com>
2025-05-23 22:35:33 +0530

    Added New Class MSRTC

---
+++ src/main/java/com/example/Makemytrip/MSRTC.java
@@ -0,0 +1,13 @@
+package com.example.Makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MSRTC {
+    public MSRTC() {
+    }
+
+    @GetMapping({"/MSRTC"})
+    public String getData() {
+        return "Please book your MSRTC BUS AT at 50% discount";
+    }
+}
